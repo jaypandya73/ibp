1d60f338d5e211481fea8515504b1b8819108b91
jay <jayved128@gmail.com>
2017-12-13 02:18:42 +0530

    setup WIP

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
 require_relative 'boot'
-
+require 'rails/all'
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
 
-require 'rails/all'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +13,7 @@ module Ibp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.autoload_paths += "#{Rails.root}/app/uploaders"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
