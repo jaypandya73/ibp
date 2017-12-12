d672e7997cf3c440aa53b1074be5d82592596d76
jay <jayved128@gmail.com>
2017-12-13 02:21:22 +0530

    resolved error

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Ibp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.autoload_paths += "#{Rails.root}/app/uploaders"
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
