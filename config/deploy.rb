a00066f31a3ec510a01330b31ef15346bc01d0ea
jay <jayved128@gmail.com>
2017-12-12 23:58:59 +0530

    setup wip

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.10.1"
 server '139.59.58.213', port: 5763, roles: [:web, :app, :db], primary: true
 
 set :application, "ibp"
-set :repo_url, "https://github.com/jaypandya73/ibp.git"
+set :repo_url, "git@github.com:jaypandya73/ibp.git"
 set :user,            'rails'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
@@ -14,7 +14,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
