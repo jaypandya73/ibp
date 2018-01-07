b1f8dfbe548490b7da33362198d81b28ea72cf08
jay <jayved128@gmail.com>
2018-01-08 00:26:08 +0530

    udpated images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @total_reserved_pxls_board = BoardPixel.pluck(:reserved_pixels).map {|b| JSON.parse(b)}.flatten(1).count
+    @total_reserved_pxls_board = BoardPixel.live.pluck(:reserved_pixels).map {|b| JSON.parse(b)}.flatten(1).count
   end
 end
